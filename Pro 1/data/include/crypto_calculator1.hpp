378ac584c8623cdd6d65f04f552027d4fa5dbd90
Arjun <YOUR_EMAIL>
2025-05-06 21:21:27 -0700

    ..

--- Pro 1/data/include/crypto_calculator1.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CRYPTO_CALCULATOR_HPP
-#define CRYPTO_CALCULATOR_HPP
-
-double calculateProfitLoss(double investment, double buyPrice, double sellPrice, int numCoins);
-void displayResults(double profitLoss);
-
-#endif
--- Pro 1/data/include/market_simulation1.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MARKET_SIMULATION_HPP
-#define MARKET_SIMULATION_HPP
-
-double getSimulatedMarketPrice();
-
-#endif
